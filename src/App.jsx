bdf7d25c1363cd0fcdc27f0b59bac935fcbc3ef9
Jeferson Macedo <jefersonmacedowgf@gmail.com>
2022-08-19 00:48:08 -0300

    Global desabilitado

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-// import './Global.css';
+import './Global.css';
 import { useContext } from 'react';
 
 
